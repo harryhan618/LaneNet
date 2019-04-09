9ca79b2f1c960c883250990142995005d83a2f78
harryhan618 <harryhan618@github.com>
2019-04-09 20:47:48 +0800

    'readme'

---
+++ net.py
@@ -0,0 +1,150 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+
+class LaneNet(nn.Module):
+    def __init__(self, embed_dim=7, pretrained=False):
+        super(LaneNet, self).__init__()
+        self.pretrained = pretrained
+        self.embed_dim = embed_dim
+        self.delta_v = .5
+        self.delta_d = 3.
+
+        self.net_init()
+
+        self.scale_seg = 1.0
+        self.scale_var = 1.0
+        self.scale_dist = 1.0
+        self.scale_reg = 0.001
+        self.seg_loss = nn.CrossEntropyLoss(weight=torch.tensor([.5, 1.]))
+
+    def net_init(self):
+        self.backbone = models.vgg16_bn(pretrained=self.pretrained).features
+
+        # ----------------- process backbone -----------------
+        for i in [34, 37, 40]:
+            conv = self.backbone._modules[str(i)]
+            dilated_conv = nn.Conv2d(
+                conv.in_channels, conv.out_channels, conv.kernel_size, stride=conv.stride,
+                padding=tuple(p * 2 for p in conv.padding), dilation=2, bias=(conv.bias is not None)
+            )
+            dilated_conv.load_state_dict(conv.state_dict())
+            self.backbone._modules[str(i)] = dilated_conv
+        self.backbone._modules.pop('33')
+        self.backbone._modules.pop('43')
+
+        # ----------------- additional conv -----------------
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(512, 1024, 3, padding=4, dilation=4, bias=False),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(),
+            nn.Conv2d(1024, 512, 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU()  # (nB, 512, 36, 100)
+        )
+
+        # ----------------- embedding -----------------
+        self.embedding = nn.Sequential(
+            nn.ConvTranspose2d(512, 256, 2, stride=2),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 128, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, self.embed_dim, 4, stride=4)
+        )
+
+        # ----------------- binary segmentation -----------------
+        self.binary_seg = nn.Sequential(
+            nn.ConvTranspose2d(512, 256, 2, stride=2),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 128, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, 2, 4, stride=4)
+        )
+
+    def forward(self, img, segLabel=None):
+        x = self.backbone(img)
+        x = self.layer1(x)
+
+        embedding = self.embedding(x)
+        binary_seg = self.binary_seg(x)
+
+        var_loss, dist_loss, reg_loss = self.discriminative_loss(embedding, segLabel)
+        seg_loss = self.seg_loss(binary_seg, torch.gt(segLabel, 0).type(torch.long))
+
+        loss = seg_loss * self.scale_seg + var_loss * self.scale_var + dist_loss * self.scale_dist + reg_loss * self.scale_reg
+
+        output = {
+            "embedding": embedding,
+            "binary_seg": binary_seg,
+            "seg_loss": seg_loss,
+            "var_loss": var_loss,
+            "dist_loss": dist_loss,
+            "reg_loss": reg_loss,
+            "loss": loss
+        }
+
+        return output
+
+    def discriminative_loss(self, embedding, seg_gt):
+        batch_size = embedding.shape[0]
+
+        var_loss = 0
+        dist_loss = 0
+        reg_loss = 0
+
+        for b in range(batch_size):
+            embedding_b = embedding[b]
+            seg_gt_b = seg_gt[b]
+
+            labels = torch.unique(seg_gt_b)
+            labels = labels[labels!=0]
+            num_lanes = len(labels)
+
+            centroid_mean = []
+            for lane_idx in labels:
+                seg_mask_i = seg_gt_b.eq(lane_idx)
+                embedding_i = embedding_b.masked_select(seg_mask_i).reshape(self.embed_dim, -1)
+
+                mean_i = torch.mean(embedding_i, dim=1)
+                centroid_mean.append(mean_i)
+
+                # ---------- var_loss -------------
+                var_loss = var_loss + torch.mean( F.relu((torch.norm(embedding_i-mean_i.reshape(self.embed_dim,1), dim=0) - self.delta_v)) **2)
+
+            centroid_mean = torch.stack(centroid_mean) # (n_lane, embed_dim)
+            centroid_mean1 = centroid_mean.reshape(-1, 1, self.embed_dim)
+            centroid_mean2 = centroid_mean.reshape(1, -1, self.embed_dim)
+            dist = torch.norm(centroid_mean1-centroid_mean2, dim=2)
+            dist_loss = dist_loss + torch.sum(F.relu(dist - self.delta_d)**2) / (num_lanes * (num_lanes-1))
+
+            reg_loss = reg_loss + torch.mean(torch.norm(centroid_mean, dim=1))
+
+        var_loss = var_loss / batch_size
+        dist_loss = dist_loss / batch_size
+        reg_loss = reg_loss / batch_size
+        return var_loss, dist_loss, reg_loss
+
+
+
+
+
+if __name__ == "__main__":
+    from dataset.CULane import CULane
+    from config import *
+    from utils.transforms import *
+
+    transform_train = Compose(Resize((800, 288)), Rotation(2), ToTensor(),
+                              Normalize(mean=(0.3598, 0.3653, 0.3662), std=(0.2573, 0.2663, 0.2756)))
+
+    dataset = CULane(CULane_path, "train", transform_train)
+    img = dataset[0]['img'].unsqueeze(0)
+    segLabel = dataset[0]['segLabel'].unsqueeze(0)
+
+    net = LaneNet()
+    o = net(img, segLabel)
+    print(o['loss'])
